eea179cde368dda466ed1670679a4431502f4681
Yubraj977 <yubrajkhatri977@gmail.com>
2024-05-14 00:05:05 +0545

    fixed the privacy policy

--- client/src/pages/Privacy-policy/Privacy.jsx
+++ client/src/pages/Privacy-policy/Privacy.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Privacy() {
     return (
-        <div className="max-w-4xl mx-auto py-8 ">
+        <div className="max-w-4xl mx-auto py-8 px-4">
             <h1 className="text-3xl font-bold mb-4">Privacy Policy</h1>
           
             <h1>Privacy Policy</h1>
