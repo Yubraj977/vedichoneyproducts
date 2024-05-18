fd2de799284b4b2a82ca2d3e8e607461efae26aa
Yubraj977 <yubrajkhatri977@gmail.com>
2024-05-18 21:16:05 +0545

    added the things

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -13,27 +13,27 @@ function Login() {
   const provider = new GoogleAuthProvider();
   const auth = getAuth(app);
 const navigate=useNavigate()
-// function handleGoogleSignin(){
-//   signInWithPopup(auth, provider)
-//   .then((result) => {
-//     // This gives you a Google Access Token. You can use it to access the Google API.
-//     const credential = GoogleAuthProvider.credentialFromResult(result);
-//     const token = credential.accessToken;
-//     // The signed-in user info.
-//     const user = result.user;
-//     // IdP data available using getAdditionalUserInfo(result)
-//     // ...
-//   }).catch((error) => {
-//     // Handle Errors here.
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // The email of the user's account used.
-//     const email = error.customData.email;
-//     // The AuthCredential type that was used.
-//     const credential = GoogleAuthProvider.credentialFromError(error);
-//     // ...
-//   });
-// }
+function handleGoogleSignin(){
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+}
 function handleGoogleSignin(){}
 function handleFacebookSignin(){
   signInWithPopup(auth, provider)
