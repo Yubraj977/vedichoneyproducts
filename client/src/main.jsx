93d94ddf47274298174432c7d761b5876c024ab6
Yubraj977 <yubrajkhatri977@gmail.com>
2024-05-31 15:23:33 +0545

    Vercel analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Analytics } from "@vercel/analytics/react"
 import { store } from '../configs/redux/store.js'
 import {Provider} from 'react-redux'
 import {PersistGate} from 'redux-persist/integration/react'
